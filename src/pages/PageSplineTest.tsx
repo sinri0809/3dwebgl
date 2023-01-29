b8e12f7d480823a73a490e3248109c71d1610258
sinri <1lliodqb@gmail.com>
2023-01-29 15:40:13 +0900

    fix: Move Spline Component  - doesn't need a specific directory

--- src/pages/PageSplineTest.tsx
+++ src/pages/PageSplineTest.tsx
@@ -1,9 +1,18 @@
-import SplineText from "components/3d/Text";
+import React, { Suspense } from 'react';
+
+const Spline = React.lazy(() => import('@splinetool/react-spline'));
 
 const PageSpline = () => {
   return <div className="spline-test">
     <h1>spline test page</h1>
-    <SplineText />
+    <Suspense fallback={<div>loading</div>}>
+      <Spline
+        className='spline-text-canvas'
+        scene="https://prod.spline.design/9aVE6044Rb1IFB1V/scene.splinecode"
+        onKeyUp={() => window.alert("onkeyup")}
+        onMouseUp={() => window.alert("onMouseUp")}
+      />
+    </Suspense>
   </div>
 }
 
