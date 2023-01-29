2ef35f7760ee4dbf3dc98e69b9ef285789ae4d25
sinri <1lliodqb@gmail.com>
2023-01-29 15:30:56 +0900

    Add SplineTest Components & package  - @splinetool

---
+++ src/pages/PageSplineTest.tsx
@@ -0,0 +1,10 @@
+import SplineText from "components/3d/Text";
+
+const PageSpline = () => {
+  return <div className="spline-test">
+    <h1>spline test page</h1>
+    <SplineText />
+  </div>
+}
+
+export default PageSpline;
--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,7 +1,9 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Home from 'pages/PageHome';
-import Board from './PageBoard';
+import Board from 'pages/PageBoard';
+import PageSpline from 'pages/PageSplineTest';
+
 import BoardContent from 'components/layout/BoardContent';
 
 import links from './links';
@@ -9,6 +11,7 @@ import links from './links';
 const Router = () => {
   return <Routes>
     <Route path={links.home} element={<Home />} />
+    <Route path={links.spline} element={<PageSpline />} />
     <Route path={links.board} element={<Board />}>
       <Route path=":id" element={<BoardContent />} />
     </Route>
