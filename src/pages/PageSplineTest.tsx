682b66bc04bd68f1008af7d7bc92631abdbeb206
SujinJeong <sujin_jeong3@tmax.co.kr>
2023-01-31 14:55:30 +0900

    feat: Add Spline-fiber example  - Add related dependencies  - ! It's not working yet

--- src/pages/PageSplineTest.tsx
+++ src/pages/PageSplineTest.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+// import useSpline from '@splinetool/r3f-spline'
 
 // import SplineReact from 'components/spline/SplineReact';
+// import Scene from 'components/spline/SplineFiber';
 
 const PageSpline = () => {
+  // const { nodes, materials } = useSpline('https://prod.spline.design/9aVE6044Rb1IFB1V/scene.splinecode')
+
   return <div className="spline-test">
     <h1>spline test page</h1>
     {/* export as React */}
     {/* <SplineReact /> */}
-
+    {/* export as react-three-fiber */}
+    {/* 
+      에러나서 못씀 https://developer.mozilla.org/ko/docs/WebAssembly/JavaScript_interface/instantiate
+      <Scene nodes={nodes} materials={materials}/> 
+    */}
   </div>
 }
 
